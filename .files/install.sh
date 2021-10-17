13e4a9bd918b5fd19bd297cb0bcbb3c366f976f2
Aprilus Lumbantoruan <i@pilus.me>
2021-10-17 11:14:09 +0700

    v2021-10-17--1114

---
+++ .files/install.sh
@@ -0,0 +1,14 @@
+if ! command -v zsh > /dev/null 2>&1 ; then
+  if ! [ $(id -u) = 0 ]; then
+    echo "We need to install zsh as root. Sudo first please..." >&2
+    exit 1
+  fi
+
+  sudo apt install -y zsh
+fi
+
+git clone https://github.com/bric3/nice-exit-code "${ZSH_CUSTOM}/plugins/nice-exit-code"
+
+if [ $(basename $SHELL) != "zsh" ]; then
+  chsh -s $(command -v zsh)
+fi
